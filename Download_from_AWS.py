f42d40f2d81bc707fcb04cd33ff19908bccacb2d
Arman Babayan <arman.babayan.92@gamil.com>
2023-10-08 22:59:51 +0400

    add s3 client, add success logger

--- Download_from_AWS.py
+++ Download_from_AWS.py
@@ -4,8 +4,8 @@ from tqdm import tqdm
 import argparse
 import boto3
 import os
-
 from helpers import timer
+
 
 load_dotenv()
 aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
@@ -14,20 +14,23 @@ bucket_name = os.getenv("BUCKET_NAME")
 bucket_folder_images = os.getenv("BUCKET_FOLDER_IMAGES")
 bucket_folder_annotations = os.getenv("BUCKET_FOLDER_ANNOTATIONS")
 
+# initialize s3 client
+s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id,
+                      aws_secret_access_key=aws_secret_access_key)
 
 @timer
-def get_number_of_files(bucket_name: str, bucket_folder: str) -> tuple:
+def get_number_of_files(s3, bucket_name: str, bucket_folder: str) -> tuple:
     """
     This function takes AWS bucket name and a folder name from that bucket
     and returns number of files and the names
+
+    :param (instance of the Boto3 S3 client) s3: s3 client
     :param str bucket_name: AWS bucket name
     :param str bucket_folder: folder name
     :return tuple: first element is number of files, second element is the names of that files
     """
-    s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id,
-                             aws_secret_access_key=aws_secret_access_key)
 
-    paginator = s3_client.get_paginator('list_objects_v2')
+    paginator = s3.get_paginator('list_objects_v2')
     response_iterator = paginator.paginate(Bucket=bucket_name, Prefix=bucket_folder)
     file_names = []
     count = 0
@@ -42,18 +45,18 @@ def get_number_of_files(bucket_name: str, bucket_folder: str) -> tuple:
 
 
 @timer
-def download_objects(object_names: list, bucket_name: str, bucket_folder: str, save_dir: str):
+def download_objects(s3, object_names: list, bucket_name: str, bucket_folder: str, save_dir: str):
     """
     This function takes object list as an input, bucket folder dir and the save path
     and downloads files from give AWS bucket to the specific directory
+
+    :param (instance of the Boto3 S3 client) s3: s3 client
     :param list object_names: name of the files to be downloaded
     :param str bucket_folder: folder path from where to be downloaded the files
     :param str save_dir: the directory where should be saved the downloaded files
     :return: None
     """
 
-    s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id,
-                      aws_secret_access_key=aws_secret_access_key)
     for object_name in tqdm(object_names):
         bucket_folder_path = '%s/%s' % (bucket_folder, object_name)
         s3.download_file(bucket_name, bucket_folder_path,
@@ -75,7 +78,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    image_count, image_names = get_number_of_files(bucket_name=args.bucket_name, bucket_folder=args.bucket_folder_images)
+    image_count, image_names = get_number_of_files(s3=s3_client, bucket_name=args.bucket_name, bucket_folder=args.bucket_folder_images)
     logger.info(f"Number of files in the folder: {image_count}")
 
     if args.number_of_files is not None:
@@ -89,16 +92,18 @@ if __name__ == '__main__':
     logger.info(f"Number of images to be downloaded: {len(image_names)}")
     logger.info(f"Number of annotations to be downloaded: {len(annotations)}")
     download_objects(
+                     s3=s3_client,
                      object_names=image_names,
                      bucket_name=args.bucket_name,
                      bucket_folder=args.bucket_folder_images,
                      save_dir=args.save_dir_images
                      )
     download_objects(
+                     s3=s3_client,
                      object_names=annotations,
                      bucket_name=args.bucket_name,
                      bucket_folder=args.bucket_folder_annotations,
                      save_dir=args.save_dir_annot
                      )
-    logger.info("Downloading finished successfully!")
+    logger.success("Downloading finished successfully!")
 
